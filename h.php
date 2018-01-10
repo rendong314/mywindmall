c8651cb42f427a5dbadae746e50024562ea96b1d
rendong314 <rendong314@163.com>
2018-01-10 10:50:52 +0800

    del

--- h.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $con=mysql_connect("127.0.0.1","webuser","7svRm1dyaqQvnlES");
-    if(!$con){
-    die("Can't connect the database ".mysql_error());
-    }
-    mysql_select_db("webuser",$con);
-?>
